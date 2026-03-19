34b16f9410aaa121ed1f80035a09a1929486c31f
balu4846 <baluvinay4846@gmail.com>
2026-03-19 17:13:40 +0530

    change

--- src/main/java/com/example/student/myStudents/dto/student.java
+++ src/main/java/com/example/student/myStudents/dto/student.java
@@ -3,60 +3,15 @@ package com.example.student.myStudents.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.util.Objects;
 
 @Entity
+@Data
 public class student {
     @Id
     private int id;
-
-    public student() {
-    }
-
     private String name;
     private int age;
-
-
-    public student(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        student student = (student) o;
-        return id == student.id && age == student.age && Objects.equals(name, student.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 }
