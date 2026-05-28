9ff0e123420a5ec8354a8f819ab4ad1b6e8f4a5b
balu4846 <baluvinay4846@gmail.com>
2026-05-28 11:05:46 +0530

    small data

--- src/main/java/com/example/student/myStudents/dto/student.java
+++ src/main/java/com/example/student/myStudents/dto/student.java
@@ -14,4 +14,6 @@ public class student {
     private int id;
     private String name;
     private int age;
+    
+    private String gmail;
 }
--- src/test/java/com/example/student/myStudents/MystudentservicesTes.java
+++ src/test/java/com/example/student/myStudents/MystudentservicesTes.java
@@ -7,7 +7,7 @@ import com.example.student.myStudents.repo.Studentrepo;
 import com.example.student.myStudents.services.Studentserices;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;g
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
